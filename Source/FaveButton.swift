dcf4d55fd200fd8ba2b2e305f80ec229d1e8a5b6
Yaroslav <yarodevuci@users.noreply.github.com>
2017-11-03 10:41:47 -0700

    Updated for Swift 4

--- Source/FaveButton.swift
+++ Source/FaveButton.swift
@@ -50,12 +50,12 @@ open class FaveButton: UIButton {
         static let dotRadiusFactors     = (first: 0.0633, second: 0.04)
     }
     
-    @IBInspectable open var normalColor: UIColor     = .darkGray//UIColor(colorLiteralRed: 137/255, green: 156/255, blue: 167/255, alpha: 1)
-    @IBInspectable open var selectedColor: UIColor   = UIColor(colorLiteralRed: 226/255, green: 38/255,  blue: 77/255,  alpha: 1)
-    @IBInspectable open var dotFirstColor: UIColor   = UIColor(colorLiteralRed: 152/255, green: 219/255, blue: 236/255, alpha: 1)
-    @IBInspectable open var dotSecondColor: UIColor  = UIColor(colorLiteralRed: 247/255, green: 188/255, blue: 48/255,  alpha: 1)
-    @IBInspectable open var circleFromColor: UIColor = UIColor(colorLiteralRed: 221/255, green: 70/255,  blue: 136/255, alpha: 1)
-    @IBInspectable open var circleToColor: UIColor   = UIColor(colorLiteralRed: 205/255, green: 143/255, blue: 246/255, alpha: 1)
+    @IBInspectable open var normalColor: UIColor     = UIColor(red: 137/255, green: 156/255, blue: 167/255, alpha: 1)
+    @IBInspectable open var selectedColor: UIColor   = UIColor(red: 226/255, green: 38/255,  blue: 77/255,  alpha: 1)
+    @IBInspectable open var dotFirstColor: UIColor   = UIColor(red: 152/255, green: 219/255, blue: 236/255, alpha: 1)
+    @IBInspectable open var dotSecondColor: UIColor  = UIColor(red: 247/255, green: 188/255, blue: 48/255,  alpha: 1)
+    @IBInspectable open var circleFromColor: UIColor = UIColor(red: 221/255, green: 70/255,  blue: 136/255, alpha: 1)
+    @IBInspectable open var circleToColor: UIColor   = UIColor(red: 205/255, green: 143/255, blue: 246/255, alpha: 1)
     
     @IBOutlet open weak var delegate: AnyObject?
     
@@ -63,14 +63,10 @@ open class FaveButton: UIButton {
     
     fileprivate var faveIconImage:UIImage?
     fileprivate var faveIcon: FaveIcon!
-    fileprivate var animationsEnabled = true
+    
     
     override open var isSelected: Bool{
         didSet{
-            guard self.animationsEnabled else {
-                return
-            }
-            
             animateSelect(self.isSelected, duration: Const.duration)
         }
     }
@@ -93,22 +89,6 @@ open class FaveButton: UIButton {
     required public init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         applyInit()
-    }
-    
-    public func setSelected(selected: Bool, animated: Bool) {
-        guard selected != self.isSelected else {
-            return
-        }
-        guard animated == false else {
-            self.isSelected = selected
-            return
-        }
-        
-        self.animationsEnabled = false
-        self.isSelected = selected
-        self.animationsEnabled = true
-        
-        animateSelect(self.isSelected, duration: 0.0) // trigger state change without animation
     }
 }
 
@@ -182,7 +162,7 @@ extension FaveButton{
         self.addTarget(self, action: #selector(toggle(_:)), for: .touchUpInside)
     }
     
-    func toggle(_ sender: FaveButton){
+    @objc func toggle(_ sender: FaveButton){
         sender.isSelected = !sender.isSelected
         
         guard case let delegate as FaveButtonDelegate = self.delegate else{
@@ -202,11 +182,7 @@ extension FaveButton{
     fileprivate func animateSelect(_ isSelected: Bool, duration: Double){
         let color  = isSelected ? selectedColor : normalColor
         
-        faveIcon.animateSelect(isSelected, fillColor: color, duration: duration, delay: duration > 0.0 ? Const.faveIconShowDelay : 0.0)
-        
-        guard duration > 0.0 else {
-            return
-        }
+        faveIcon.animateSelect(isSelected, fillColor: color, duration: duration, delay: Const.faveIconShowDelay)
         
         if isSelected{
             let radius           = bounds.size.scaleBy(1.3).width/2 // ring radius
@@ -226,8 +202,6 @@ extension FaveButton{
         }
     }
 }
-
-
 
 
 
